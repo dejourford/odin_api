ba049adb522cb24370380866b6f34347f13909dd
dejourford <dejourford@my.unt.edu>
2026-02-16 23:57:39 -0600

    get search query from input value

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 // listener for sumbit button
 const searchButton = document.querySelector("#search-button")
 searchButton.addEventListener("click", () => {
-    
-} )
+    console.log("search button clicked")
+})
 
 // listener for form
+let query;
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
    e.preventDefault();
     console.log("form submit")
+
+    // get query from input value
+    const formData = new FormData(form);
+    query = formData.get("search");
+    console.log(query)
 })
